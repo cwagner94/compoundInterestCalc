c02daabbe570af0fc449ed57c384316a9dda2896
Chris Wagner <chriswagner@wagnerchKMD6R.vmware.com>
2023-03-27 19:49:40 -0500

    Make changes to input boxes

--- frontend/src/components/SelectField.jsx
+++ frontend/src/components/SelectField.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SelectFieldOption from './SelectFieldOption'
 import '../CSS/SelectField.css'
+import '../CSS/InputField.css'
 
 function SelectField(props) {
     return (
         <div className="inputBox">
-            <select name={props.name} id={props.id} onChange={props.onChange}>
+            <select className="selectBox" name={props.name} id={props.id} onChange={props.onChange}>
                 <SelectFieldOption value={"Annually"} text={"Annually"} />
                 <SelectFieldOption value={"Semiannually"} text={"Semiannually"} />
                 <SelectFieldOption value={"Quarterly"} text={"Quarterly"} />
